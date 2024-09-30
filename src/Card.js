8e997f20be26fd7cdcacafb3217ca0b1caf1ab7f
cipta anugrah <ciptaanugrahh@gmail.com>
2024-09-30 15:26:17 +0700

    ini uploadan pertama ke github

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = (props) => {
+    return (
+        <div className='tc bg-light-green dib br3 pa3 ma3 grow bw2 shadow-5'>
+            <img src={`https://robohash.org/${props.id}?200x200`} alt='robots' />
+            <div>
+                 <h3>{props.name}</h3>
+                  <p>{props.email}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
